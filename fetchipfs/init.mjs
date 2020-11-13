8a5e9e05204bc0c631b4c8f744e140f478636d1c
Phil Wornath <philipp@wornath.de>
2020-11-13 21:36:31 +0100

    Update init.mjs

--- fetchipfs/init.mjs
+++ fetchipfs/init.mjs
@@ -109,5 +109,5 @@ async function start(ghpath) {
 		todelete[i].parentNode.removeChild(todelete[i])
 }
 
-start('https://github.com/openflasq/openflasq-web/');
+start('https://openflasq.github.io/openflasq-web/');
 
